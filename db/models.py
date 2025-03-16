e097a698a5e26c5f5e95782bafedf54477bcbd08
fanni <fmalevych@zoolatech.com>
2025-03-16 22:17:20 +0200

    add models

---
+++ db/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Integer, Column, String, ForeignKey, Date
+from sqlalchemy.orm import relationship
+
+from db.engine import Base
+
+
+class DBBook(Base):
+    __tablename__ = "book"
+
+    id = Column(Integer, index=True, primary_key=True)
+    title = Column(String(255), nullable=False)
+    summary = Column(String(511), nullable=False)
+    publication_date = Column(Date, nullable=False)
+    author_id = Column(Integer, ForeignKey("author.id"))
+
+
+class DBAuthor(Base):
+    __tablename__ = "author"
+
+    id = Column(Integer, index=True, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    bio = Column(String(511), nullable=False)
+    books = relationship(DBBook)
