af498a5f5454c58245379849520c292c32ce5f5d
fanni <fmalevych@zoolatech.com>
2025-03-16 22:35:35 +0200

    add schemas

---
+++ schemas.py
@@ -0,0 +1,37 @@
+import datetime
+
+from pydantic import BaseModel
+
+
+class BookBase(BaseModel):
+    title: str
+    summary: str
+    publication_date: datetime.date
+    author_id: int
+
+
+class BookCreate(BookBase):
+    pass
+
+
+class Book(BookBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class AuthorBase(BaseModel):
+    name: str
+    bio: str
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class Author(AuthorBase):
+    id: int
+
+    class Config:
+        orm_mode = True
