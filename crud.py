4bdcd7d1bb75c1d918ab9756e2e818df720357bf
fanni <fmalevych@zoolatech.com>
2025-03-16 22:35:48 +0200

    add crud operations

---
+++ crud.py
@@ -0,0 +1,46 @@
+from sqlalchemy.orm import Session
+
+import schemas
+from db import models
+
+
+def get_all_authors(db: Session):
+    return db.query(models.DBAuthor).all()
+
+
+def create_author(db: Session, author: schemas.AuthorCreate):
+    db_author = models.DBAuthor(
+        name=author.name,
+        bio=author.bio,
+    )
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+
+    return db_author
+
+
+def get_author_by_name(db: Session, name: str):
+    return db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
+
+
+def get_all_books(db: Session):
+    return db.query(models.DBBook).all()
+
+
+def create_book(db: Session, book: schemas.BookCreate):
+    db_book = models.DBBook(
+        title=book.title,
+        summary=book.summary,
+        publication_date=book.publication_date,
+        author_id=book.author_id,
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+
+    return db_book
+
+
+def get_book_by_title(db: Session, title: str):
+    return db.query(models.DBBook).filter(models.DBBook.title == title).first()
